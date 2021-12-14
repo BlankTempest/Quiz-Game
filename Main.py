6e603e32930919be302d33b0e2d18d1106bd53f9
BlankTempest <61834898+BlankTempest@users.noreply.github.com>
2021-12-14 19:25:13 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,6 +5,8 @@ pygame.init()                                             #initializes pygame
 display = pygame.display.set_mode((800,600))
 pygame.display.update()                                   #updates the window with new dimensions
 
+pygame.display.set_caption('Game window xxx')             #set the window name  
+
 open = True
 while open:
     for event in pygame.event.get():
